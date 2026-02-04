18934001daff8a517d30985bc6dffcd52c64fd74
bradyapps <bradyraule@gmail.com>
2026-02-04 16:37:05 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/bradyapps/hospital_ml_triage/HospitalMlTriageApplication.java
@@ -0,0 +1,13 @@
+package com.bradyapps.hospital_ml_triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMlTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMlTriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bradyapps/hospital_ml_triage/HospitalMlTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradyapps.hospital_ml_triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMlTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
